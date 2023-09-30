7e7aa5e227ba98181fed6e6fcc204daef0569914
In Ho Jong <in391@outlook.com>
2023-09-30 17:10:46 +0900

    Crontab is utilized

--- Data_record.py
+++ Data_record.py
@@ -1,23 +1,13 @@
 import Data_read
 import csv
-import time, datetime
+import time
 
 bus = Data_read.bus_start()
-date = datetime.datetime.today()
-minute = date.minute
+Data_read.collect_start(bus)
+time.sleep(1)
 
-while True:
-	date = datetime.datetime.today()
-	if minute is not date.minute:
-		Data_read.collect_start(bus)
-		time.sleep(1)
-		temp_comp = Data_read.collect_temp(bus)
-		hum_comp = Data_read.collect_humid(bus, temp_comp)
-		date = datetime.datetime.today()
-		minute = date.minute
-		with open('data/data_temp_humid_2.csv', 'a') as file:
-			writer = csv.writer(file)
-			writer.writerow([time.time(), temp_comp/100, hum_comp/10000])
-		#print(temp_comp/100)
-		#print(hum_comp/10000)
-	time.sleep(10)
+temp_comp = Data_read.collect_temp(bus)
+hum_comp = Data_read.collect_humid(bus, temp_comp)
+with open('data/data_temp_humid_3.csv', 'a') as file:
+	writer = csv.writer(file)
+	writer.writerow([time.time(), temp_comp/100, hum_comp/10000])
